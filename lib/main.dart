48aa2577fee97db4448162a41b69ab99079ec2e9
donghyukkil <asterism90@gmail.com>
2024-09-22 12:30:03 +0900

    feat: Conditionally handle GitHub API requests based on token availability

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,19 @@
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 import 'repositories/data_provider.dart';
 import 'repositories/github_repository.dart';
 import 'screens/home_screen.dart';
+import 'bloc/users/github_users_bloc.dart';
 
-void main() {
+void main() async {
+  await dotenv.load(fileName: ".env");
   final dio = Dio();
-  final apiProvider = GithubApiProvider(dio);
+
+  final apiToken = dotenv.env['GITHUB_API_TOKEN'] ?? '';
+  final apiProvider = GithubApiProvider(dio, apiToken);
   final githubRepository = GithubRepository(apiProvider);
 
   runApp(MyApp(githubRepository: githubRepository));
@@ -16,16 +22,19 @@ void main() {
 class MyApp extends StatelessWidget {
   final GithubRepository githubRepository;
 
-  const MyApp({required this.githubRepository});
+  const MyApp({super.key, required this.githubRepository});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'GitHub Users App',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return BlocProvider(
+      create: (context) => GithubUsersBloc(githubRepository),
+      child: MaterialApp(
+        title: 'GitHub Users App',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: HomeScreen(githubRepository: githubRepository),
       ),
-      home: HomeScreen(githubRepository: githubRepository),
     );
   }
 }
--- lib/repositories/data_provider.dart
+++ lib/repositories/data_provider.dart
@@ -2,16 +2,22 @@ import 'package:dio/dio.dart';
 
 class GithubApiProvider {
   final Dio _dio;
+  final String? _token;
 
-  GithubApiProvider(this._dio);
+  GithubApiProvider(this._dio, [this._token = '']);
 
   Future<Response> fetchUsers(int? since) async {
+    final options = Options(headers: {
+      if (_token!.isNotEmpty) 'Authorization': 'token $_token',
+    });
+
     return await _dio.get(
       'https://api.github.com/users',
       queryParameters: {
         if (since != null) 'since': since,
         'per_page': 10,
       },
+      options: options,
     );
   }
 }
